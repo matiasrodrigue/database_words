92ad33ece129d88afb1ab7fdbc303cae9f983a64
matiasrodrigue <88112759+matiasrodrigue@users.noreply.github.com>
2024-09-29 02:41:09 -0300

    Ultimo cambio order by

--- 04_Order_by.sql
+++ 04_Order_by.sql
@@ -1,3 +1,5 @@
 SELECT * FROM actor order by first_name; -- Oderna de A, B, C ....
 
 SELECT * FROM actor order by first_name desc; -- Esto es de manera descendente  C, B, A
+
+SELECT * FROM actor where last_name = "RODRIGUEZ" order by first_name desc;
