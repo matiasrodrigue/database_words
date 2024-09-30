79974dd1fed3913a5a5994bf5d4a08d09a96c1f0
matiasrodrigue <88112759+matiasrodrigue@users.noreply.github.com>
2024-09-30 01:11:10 -0300

    new

---
+++ 08_Null.sql
@@ -0,0 +1,4 @@
+Select * from actor where actor_id is null ; -- Trae todos los id que no tienen valor osea que estan NULL
+
+Select * from actor where actor_id is not null ; -- Trae todos los valores NULL
+ 
