6eed365ff470931f578b5a81e472958c73d874a9
matiasrodrigue <88112759+matiasrodrigue@users.noreply.github.com>
2024-10-12 23:58:00 -0300

    Ultimo cambio

--- 27_Default.sql
+++ 27_Default.sql
@@ -1,6 +1,7 @@
-create table contactos(
+create table contactos_2(
 num_contacto int primary key,
-nombre_contacto varchar(25) default,
+nombre_contacto varchar(25) default "NN", 
 edad_contacto int,
+Dia_alta datetime default current_timestamp, -- Si no pongo fecha me trae la fecha del momento en que se creo
 check (edad_contacto >= 18) -- restringe que la edad de los contactos sea igual o mayor a 18 años
 );
