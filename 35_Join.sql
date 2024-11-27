a36d000eddd74c2be922fb631270e7a6549e57fb
matiasrodrigue <88112759+matiasrodrigue@users.noreply.github.com>
2024-11-27 00:45:10 -0300

    change

---
+++ 35_Join.sql
@@ -0,0 +1,4 @@
+use my_table; -- tabla que usas para los ejemplos
+
+select * from dueños_de_mascotas
+inner join documentos_de_identidad; -- Aqui traigo todos los datos de las tablas "dueños_de_mascotas" y "documentos_de_identidad"
