8c52c40a53eae3b1c8c366f56fffc2ebf02b9073
matiasrodrigue <88112759+matiasrodrigue@users.noreply.github.com>
2024-11-28 01:57:39 -0300

    update

--- 35_Join.sql
+++ 35_Join.sql
@@ -8,3 +8,16 @@ inner join documentos_de_identidad -- Aqui traigo todos los datos de las tablas 
 on dueños_de_mascotas.id_mascota = documentos_de_identidad.id_mascota -- juntas la informacion de dos tablas "dueños_de_mascotas" y "documentos_de_identidad"
 inner join medico_veterinario
 on medico_veterinario.dni= documentos_de_identidad.dni;
+
+
+
+
+
+-- Este ejemplo solo trae nombre de mascota y nombre de dueño de mascota
+
+
+select dueños_de_mascotas.nombre_mascota, documentos_de_identidad.nome from dueños_de_mascotas
+inner join documentos_de_identidad -- Aqui traigo todos los datos de las tablas "dueños_de_mascotas" y "documentos_de_identidad"
+on dueños_de_mascotas.id_mascota = documentos_de_identidad.id_mascota -- juntas la informacion de dos tablas "dueños_de_mascotas" y "documentos_de_identidad"
+inner join medico_veterinario
+on medico_veterinario.dni= documentos_de_identidad.dni;
