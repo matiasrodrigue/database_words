8e56712b10c81b06203755bab374c5d766f61ec8
matiasrodrigue <88112759+matiasrodrigue@users.noreply.github.com>
2024-11-28 01:50:39 -0300

    joinear

--- 35_Join.sql
+++ 35_Join.sql
@@ -1,4 +1,10 @@
-use my_table; -- tabla que usas para los ejemplos
+use my_table;
+
+/*inner join*/
+use my_table;
 
 select * from dueños_de_mascotas
-inner join documentos_de_identidad; -- Aqui traigo todos los datos de las tablas "dueños_de_mascotas" y "documentos_de_identidad"
+inner join documentos_de_identidad -- Aqui traigo todos los datos de las tablas "dueños_de_mascotas" y "documentos_de_identidad"
+on dueños_de_mascotas.id_mascota = documentos_de_identidad.id_mascota -- juntas la informacion de dos tablas "dueños_de_mascotas" y "documentos_de_identidad"
+inner join medico_veterinario
+on medico_veterinario.dni= documentos_de_identidad.dni;
