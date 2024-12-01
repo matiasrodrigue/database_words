a9d0569486b5056d55b2ea15974c332a0453023c
matiasrodrigue <88112759+matiasrodrigue@users.noreply.github.com>
2024-12-01 00:58:57 -0300

    Dereita

--- 35_Join.sql
+++ 35_Join.sql
@@ -24,3 +24,10 @@ on medico_veterinario.dni= documentos_de_identidad.dni;
 select * from dueños_de_mascotas
 left join documentos_de_identidad -- Aqui traigo todos los datos de las tablas "dueños_de_mascotas" y los que no esten en comun en "documentos_de_identidad"  traigo el campo como null
 on dueños_de_mascotas.id_mascota = documentos_de_identidad.id_mascota; -- juntas la informacion de dos tablas "dueños_de_mascotas" y "documentos_de_identidad"
+
+/*Righ join*/
+
+select * from dueños_de_mascotas
+right join documentos_de_identidad -- Aqui traigo todos los datos de las tablas "dueños_de_mascotas" y los que no esten en comun en "documentos_de_identidad" en comun, priorizando la tabla de la derecha
+on dueños_de_mascotas.id_mascota = documentos_de_identidad.id_mascota; -- juntas la informacion de dos tablas "dueños_de_mascotas" y "documentos_de_identidad"
+
