78bbca143ea6f35e29d578d15beb2757f258dd7f
matiasrodrigue <88112759+matiasrodrigue@users.noreply.github.com>
2024-11-30 23:39:45 -0300

    Update

--- 35_Join.sql
+++ 35_Join.sql
@@ -21,3 +21,9 @@ inner join documentos_de_identidad -- Aqui traigo todos los datos de las tablas 
 on dueños_de_mascotas.id_mascota = documentos_de_identidad.id_mascota -- juntas la informacion de dos tablas "dueños_de_mascotas" y "documentos_de_identidad"
 inner join medico_veterinario
 on medico_veterinario.dni= documentos_de_identidad.dni;
+
+
+/*left join*/
+
+-- Test
+
