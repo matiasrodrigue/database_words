cf168403aeaea85b93b72e6c6c6a4ca1d90f616a
matiasrodrigue <88112759+matiasrodrigue@users.noreply.github.com>
2024-12-29 02:27:39 -0300

    View

---
+++ 38_View.sql
@@ -0,0 +1,9 @@
+
+-- Aqui creo una vista donde me muestre las mascotas cuyo id es >= 1; tambien podria hacerse con edades o cualquier consulta que sea habitual
+
+create view v_nombre_de_macotas as
+select nombre_mascota
+from dueños_de_mascotas
+where id_mascota >= 2;
+
+Select * from v_nombre_de_macotas;
