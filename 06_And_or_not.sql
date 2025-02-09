8f1151f8e32c67607280cdbe0bbba625187a755a
matiasrodrigue <88112759+matiasrodrigue@users.noreply.github.com>
2025-02-09 19:08:04 -0300

    Depuracion

--- 06_And_or_not.sql
+++ 06_And_or_not.sql
@@ -1,4 +1,4 @@
-Select * from actor where not actor_id = 1; -- Trae todos los id menos el 1 utilizando NOR
+Select * from actor where not actor_id = 1; -- Trae todos los id menos el 1 utilizando NOT
 
 Select * from actor where not actor_id = 1 and first_name = "JOHNNY"; -- Aqui sume la condicion AND a la formula anterior
 
